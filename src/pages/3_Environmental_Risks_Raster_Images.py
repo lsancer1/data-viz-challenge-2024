49024c96f4ec2c8510cb75c23bcda8f6ab0dce46
lsancer1 <lsancer1@smail.uni-koeln.de>
2024-10-18 01:05:14 +0200

    Play with dimensions of raster images

--- src/pages/3_Environmental_Risks_Raster_Images.py
+++ src/pages/3_Environmental_Risks_Raster_Images.py
@@ -539,7 +539,7 @@ with tab1:
 		# st.plotly_chart(fig, use_container_width=True)
 		# st.image(temp_img, caption="Temperature Forecast Map", use_column_width=True)
 
-		st.image("https://i.imgur.com/MWch7ZP.png", caption="Corsica Map", height=1200, width=600)
+		st.image("https://i.imgur.com/MWch7ZP.png", caption="Corsica Map", width=600)
 
 
 		# plotly_img = Image.open("plotly_fig.png") 
