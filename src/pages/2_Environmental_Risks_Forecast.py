17faee7c8592607eff9163bac2c17b12cae94c98
lsancer1 <lsancer1@smail.uni-koeln.de>
2024-10-18 03:52:29 +0200

    Create forecast tabs

--- src/pages/2_Environmental_Risks_Forecast.py
+++ src/pages/2_Environmental_Risks_Forecast.py
@@ -597,7 +597,7 @@ with tab1:
 
 		# Apply transparency (set alpha) to the help image
 		temp_img = temp_img.convert("RGBA")  # Ensure it's in RGBA mode for transparency
-		alpha = 0.25  # Adjust transparency level (0 is fully transparent, 1 is fully opaque)
+		alpha = 0.75  # Adjust transparency level (0 is fully transparent, 1 is fully opaque)
 		temp_img.putalpha(int(255 * alpha))
 
 		# Merge the two images
@@ -605,6 +605,33 @@ with tab1:
 
 		# Display the final combined image
 		st.image(combined_img, caption="Put caption here")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 		# st.image(corsica_map, caption="corsica_map", use_column_width=True)
